1185f0f15e683a4f8d736b6e3a5746ab4d53ad33
des1177 <yinwu@ucdavis.edu>
2021-08-15 19:59:20 -0700

    Update sunflower.java

--- sunflower.java
+++ sunflower.java
@@ -1,5 +1,35 @@
 
-// find height of a binary tree
+
+
+
+// in-order traversal print a binary tree
+void inOrder (TreeNode root) {
+      //base case
+      if(root == null) {
+        return;      
+      }
+      //recursive rule
+      inOrder(root.left);
+      System.out.println(root.val);
+      inOrder(root,right);
+}
+--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+// pre-order traversal print a binary tree
+void preOrder (TreeNode root) {
+      //base case
+      if(root == null) {
+        return;      
+      }
+      //recursive rule
+      System.out.println(root.val);
+      preOrder(root.left);
+      preOrder(root,right);
+}
+
+--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+// find height of a binary tree -
 public int maxDepth(TreeNode root) {
       // base case
       if(root == null) {
@@ -9,3 +39,6 @@ public int maxDepth(TreeNode root) {
       int height = Math.max(maxDepth(root.left),maxDepth(root.right))+1;
       return height;
     }
+
+--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
