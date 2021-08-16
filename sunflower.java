350ee3ea5fa2fb8ac7d8771974c10a3692c6b73e
des1177 <yinwu@ucdavis.edu>
2021-08-15 19:40:58 -0700

    Create sunflower.java

---
+++ sunflower.java
@@ -0,0 +1,9 @@
+public int maxDepth(TreeNode root) {
+      // base case
+      if(root == null) {
+        return 0;
+      }
+      // resursive rule 
+      int height = Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+      return height;
+    }
